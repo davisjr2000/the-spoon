579df1c71cff38965a3fc4c684df3971cfeb8c4f
Davis JR <davisjr2000@hotmail.com>
2019-08-20 11:56:08 -0300

    Complete CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,38 @@
 Rails.application.routes.draw do
   get '/home', to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # CRUD Restaurant
+
+  # CREATE
+
+  get "/restaurants/new", to: "restaurants#new"
+
+  post '/restaurants', to: 'restaurants#create'
+
+  # READ
+
+  # see all restaurants
+
+  get "/restaurants", to: "restaurants#index"
+
+  # see one restaurant
+
+  get "/restaurants/:id", to: 'restaurants#show', as: "restaurant"
+
+  # UPDATE
+
+  # form for editing a restaurant
+
+  get "/restaurants/:id/edit", to: 'restaurants#edit', as: "edit_restaurant"
+
+  # update a restaurant
+
+  patch "/restaurants/:id", to: 'restaurants#update'
+
+  # DELETE
+
+  delete 'restaurants/:id', to: 'restaurants#destroy'
+
+  # resources :restaurants
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create(name: "Bullguer", rating: 5, address: "Fradique Coutinho 1234")
+
+Restaurant.create(name: "Siri Cascudo", rating: 3, address: "Fenda do Biquini")
